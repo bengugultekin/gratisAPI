3a291e4368cfe52c748eb59a9db373d8a23fd14a
Bengünur Gültekin <bengunurgultekin54@gmail.com>
2020-02-28 23:33:27 +0300

    Added Category Model

---
+++ Model/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace gratisAPI.Model
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(60, MinimumLength =3)]
+        public string Title { get; set; }
+
+        public List<Category> SubCategories;
+    }
+}
